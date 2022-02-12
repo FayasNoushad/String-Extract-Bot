b7ab0990c66aa8dc52f2f47187f15ad62536371e
Fayas Noushad <76828314+FayasNoushad@users.noreply.github.com>
2022-02-12 12:11:39 +0530

    Update code

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import os
-import string_extract
-import hashtags_extract
-import domain_extract
 from pyrogram import Client, filters
+import string_extract, hashtags_extract, domain_extract
 
 
 Bot = Client(
     "String-Extract-Bot",
-    bot_token = os.environ.get("BOT_TOKEN"),
-    api_id = int(os.environ.get("API_ID")),
-    api_hash = os.environ.get("API_HASH")
+    bot_token=os.environ.get("BOT_TOKEN"),
+    api_id=int(os.environ.get("API_ID")),
+    api_hash=os.environ.get("API_HASH")
 )
 
 types = [
@@ -26,9 +24,12 @@ types = [
 
 @Bot.on_message(filters.command("start"))
 async def start(bot, update):
-    text = f"Hi {update.from_user.mention}, I am a simple string extract bot." + "\n\n" +
+    
+    text = f"Hi {update.from_user.mention}," + "\n" +
+    "I am a simple string extract bot." + "\n\n" +
     "Send /extract {type} as reply a message for extracting" + "\n\n" +
     "**Types:**" + ", ".join(types) + "\n\n" + "Made by @FayasNoushad"
+    
     await update.reply_text(
         text=text,
         quote=True
@@ -37,13 +38,17 @@ async def start(bot, update):
 
 @Bot.on_message(filters.command("extract"))
 async def extract(bot, update):
+    
     if " " not in update.text or not update.reply_to_message.text or not update.reply_to_message.caption:
         await update.reply_text("Please send command with type as reply to a string")
+    
     else:
         type = update.text.split()[1]
         string = update.reply_to_message.text if update.reply_to_message.text else update.reply_to_message.caption
+        
         if type not in types:
             await update.reply_text("`Invalid type!`")
+        
         else:
             if type == "lines":
                 text = string_extract.lines(string)
@@ -61,6 +66,7 @@ async def extract(bot, update):
                 text = "\n".join(string_extract.urls(string))
             elif type == "domains":
                 text = "\n".join(domain_extract.string_domains(string))
+            
             await update.reply_text(
                 text=str(text),
                 quote=True,
